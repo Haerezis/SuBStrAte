1c71984d110121f84c2ecb30019ab31c4e97b680
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2015-03-05 15:10:32 +0100

    Add OpenScop library to the repository as git submodule and modify CMakeLists to link to osl

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 SuBStrAte, the Similar Behaving Statements Aggregator.
+
+To compile SuBStrAte, you need to have the OpenScop library installed.
+If it is not the case, you can get the latest version with the command
+
+```
+git submodule update --init
+```
+
+To compile :
+```
+cmake .
+make
+```
--- src/substrate.c
+++ src/substrate.c
@@ -1,9 +1,15 @@
-#include<stdio.h>
-#include "substrate.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <substrate.h>
+#include <osl/scop.h>
+#include <osl/macros.h>
+#include <osl/util.h>
+#include <osl/extensions/dependence.h>
+
 
 int main(int argc,char** argv)
 {
     printf("Hello World !\n");
-
     return 0;
 }
