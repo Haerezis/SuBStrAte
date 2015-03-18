ff535301b42fef8da5234c347ccd84d5dc999c16
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2015-03-18 16:20:27 +0100

    add substrate_*_add and substrate_*_clone functions for reuse structures add function add two structure into a third one, recursively for every sub structure clone functions just allocate and make copy of the argument

--- include/reuse_struct.h
+++ include/reuse_struct.h
@@ -33,6 +33,14 @@ struct substrate_reuse_profile
 };
 
 
+struct substrate_reuse_profile substrate_reuse_profile_clone(
+        struct substrate_reuse_profile rp);
+struct substrate_array_profile substrate_array_profile_clone(
+        struct substrate_array_profile ap);
+struct substrate_uniformly_generated_set substrate_uniformly_generated_set_clone(
+        struct substrate_uniformly_generated_set ugs);
+struct substrate_equivalence_class substrate_equivalence_class_clone(
+        struct substrate_equivalence_class ec);
 
 void substrate_reuse_profile_free(
         struct substrate_reuse_profile * rp);
@@ -56,4 +64,20 @@ void substrate_uniformly_generated_set_dump(
 void substrate_equivalence_class_dump(
         FILE * output_stream,
         struct substrate_equivalence_class * ec);
+
+
+struct substrate_reuse_profile substrate_reuse_profile_add(
+        struct substrate_reuse_profile rp1,
+        struct substrate_reuse_profile rp2);
+struct substrate_array_profile substrate_array_profile_add(
+        struct substrate_array_profile ap1,
+        struct substrate_array_profile ap2);
+struct substrate_uniformly_generated_set substrate_uniformly_generated_set_add(
+        struct substrate_uniformly_generated_set ugs1,
+        struct substrate_uniformly_generated_set ugs2);
+struct substrate_equivalence_class substrate_equivalence_class_add(
+        struct substrate_equivalence_class ec1,
+        struct substrate_equivalence_class ec2);
+
+
 #endif
--- src/reuse_struct.c
+++ src/reuse_struct.c
@@ -1,6 +1,77 @@
 #include <stdlib.h>
 
+#include "osl/osl.h"
+
 #include "reuse_struct.h"
+#include "utils.h"
+
+struct substrate_reuse_profile substrate_reuse_profile_clone(
+        struct substrate_reuse_profile rp)
+{
+    unsigned int i = 0;
+    struct substrate_reuse_profile res;
+
+    res.array_profiles = (struct substrate_array_profile*)
+        malloc(rp.size * sizeof(struct substrate_array_profile));
+    res.size = rp.size;
+    for(i=0 ; i<res.size ; i++)
+    {
+        res.array_profiles[i] = substrate_array_profile_clone(rp.array_profiles[i]);
+    }
+
+    return res;
+}
+
+struct substrate_array_profile substrate_array_profile_clone(
+        struct substrate_array_profile ap)
+{
+    unsigned int i = 0;
+    struct substrate_array_profile res;
+   
+    
+    res.array_id_precision = ap.array_id_precision;
+    osl_int_init_set(res.array_id_precision, &res.array_id, ap.array_id);
+    res.uniformly_gen_sets = (struct substrate_uniformly_generated_set*)
+        malloc(ap.size * sizeof(struct substrate_uniformly_generated_set));
+    res.size = ap.size;
+    for(i=0 ; i<res.size ; i++)
+    {
+        res.uniformly_gen_sets[i] = substrate_uniformly_generated_set_clone(
+                ap.uniformly_gen_sets[i]);
+    }
+
+    return res;
+}
+
+struct substrate_uniformly_generated_set substrate_uniformly_generated_set_clone(
+        struct substrate_uniformly_generated_set ugs)
+{
+    unsigned int i = 0;
+    struct substrate_uniformly_generated_set res;
+
+    res.H_matrix = osl_relation_clone(ugs.H_matrix);
+    res.classes = (struct substrate_equivalence_class*)
+        malloc(ugs.size * sizeof(struct substrate_equivalence_class));
+    res.size = ugs.size;
+    for(i=0 ; i<res.size ; i++)
+    {
+        res.classes[i] = substrate_equivalence_class_clone(ugs.classes[i]);
+    }
+
+    return res;
+}
+
+struct substrate_equivalence_class substrate_equivalence_class_clone(
+        struct substrate_equivalence_class ec)
+{
+    struct substrate_equivalence_class res;
+
+    res.array_references = osl_relation_list_clone(ec.array_references);
+    res.size = ec.size;
+
+    return res;
+}
+
 
 void substrate_reuse_profile_free(
         struct substrate_reuse_profile * rp)
@@ -106,4 +177,203 @@ void substrate_equivalence_class_dump(
     fprintf(output_stream,"\t\tNumber of references in the class : %d\n",ec->size);
     fprintf(output_stream,"\n");
     osl_relation_list_idump(output_stream,ec->array_references,3);
+}
+
+
+
+struct substrate_reuse_profile substrate_reuse_profile_add(
+        struct substrate_reuse_profile rp1,
+        struct substrate_reuse_profile rp2)
+{
+    unsigned int i=0, i1 = 0, i2 = 0;
+    unsigned int max_size = 0, res_size = 0;
+    struct substrate_reuse_profile res;
+    
+    max_size = rp1.size + rp2.size;
+    res.array_profiles = (struct substrate_array_profile*)
+        malloc(max_size * sizeof(struct substrate_array_profile));
+
+    for(i1=0 ; i<rp1.size ; i1++)
+    {
+        for(i2=0; i2<rp2.size ; i2++)
+        {
+            if(osl_int_eq(
+                        rp1.array_profiles[i1].array_id_precision,
+                        rp1.array_profiles[i1].array_id,
+                        rp2.array_profiles[i2].array_id))
+            {
+                res.array_profiles[res.size] = 
+                    substrate_array_profile_add(
+                            rp1.array_profiles[i1],
+                            rp2.array_profiles[i2]);
+                res.size++;
+            }
+        }
+        if(i2 >= rp2.size)
+        {
+            res.array_profiles[res.size] = 
+                substrate_array_profile_clone(rp1.array_profiles[i1]);
+            res.size++;
+        }
+    }
+
+    res_size = res.size;
+    for(i2=0 ; i2<rp2.size ; i2++)
+    {
+        for(i=0 ; i<res_size ; i++)
+        {
+            if(osl_int_eq(
+                        rp1.array_profiles[i1].array_id_precision,
+                        rp1.array_profiles[i1].array_id,
+                        rp2.array_profiles[i2].array_id))
+            {
+                break;
+            }
+        }
+        if(i >= res_size)
+        {
+            res.array_profiles[res.size] = 
+                substrate_array_profile_clone(rp2.array_profiles[i2]);
+            res.size++;
+        }
+    }
+
+    return res;
+}
+
+struct substrate_array_profile substrate_array_profile_add(
+        struct substrate_array_profile ap1,
+        struct substrate_array_profile ap2)
+{
+    unsigned int i=0, i1 = 0, i2 = 0;
+    unsigned int max_size = 0, res_size = 0;
+    struct substrate_array_profile res;
+    
+    max_size = ap1.size + ap2.size;
+    res.array_id_precision = ap1.array_id_precision;
+    osl_int_init_set(res.array_id_precision, &res.array_id, ap1.array_id);
+    res.uniformly_gen_sets = (struct substrate_uniformly_generated_set*)
+        malloc(max_size * sizeof(struct substrate_uniformly_generated_set));
+
+    for(i1=0 ; i<ap1.size ; i1++)
+    {
+        for(i2=0; i2<ap2.size ; i2++)
+        {
+            if(substrate_H_matrix_eq(
+                        ap1.uniformly_gen_sets[i1].H_matrix,
+                        ap2.uniformly_gen_sets[i2].H_matrix))
+            {
+                res.uniformly_gen_sets[res.size] = 
+                    substrate_uniformly_generated_set_add(
+                            ap1.uniformly_gen_sets[i1],
+                            ap2.uniformly_gen_sets[i2]);
+                res.size++;
+            }
+        }
+        if(i2 >= ap2.size)
+        {
+            res.uniformly_gen_sets[res.size] = 
+                substrate_uniformly_generated_set_clone(ap1.uniformly_gen_sets[i1]);
+            res.size++;
+        }
+    }
+
+    res_size = res.size;
+    for(i2=0 ; i2<ap2.size ; i2++)
+    {
+        for(i=0 ; i<res_size ; i++)
+        {
+            if(substrate_access_class_eq(
+                        res.uniformly_gen_sets[i1].H_matrix,
+                        ap2.uniformly_gen_sets[i2].H_matrix))
+            {
+                break;
+            }
+        }
+        if(i >= res_size)
+        {
+            res.uniformly_gen_sets[res.size] = 
+                substrate_uniformly_generated_set_clone(ap2.uniformly_gen_sets[i2]);
+            res.size++;
+        }
+    }
+
+    return res;
+}
+
+struct substrate_uniformly_generated_set substrate_uniformly_generated_set_add(
+        struct substrate_uniformly_generated_set ugs1,
+        struct substrate_uniformly_generated_set ugs2)
+{
+    struct substrate_uniformly_generated_set res;
+    unsigned int i=0, i1 = 0, i2 = 0;
+    unsigned int max_size = 0, res_size = 0;
+
+    max_size = ugs1.size + ugs2.size;
+    res.H_matrix = osl_relation_clone(ugs1.H_matrix);
+    res.size = 0;
+    res.classes = (struct substrate_equivalence_class*)
+        malloc(max_size * sizeof(struct substrate_equivalence_class));
+
+    for(i1=0 ; i1<ugs1.size ; i1++)
+    {
+        for(i2=0 ; i2<ugs2.size ; i2++)
+        {
+
+            if(substrate_access_class_eq(
+                        ugs1.classes[i1].array_references->elt,
+                        ugs2.classes[i2].array_references->elt))
+            {
+                res.classes[res.size] = substrate_equivalence_class_add(
+                        ugs1.classes[i1],
+                        ugs2.classes[i2]);
+                res.size++;
+                break;
+            }
+        }
+        if(i2 >= ugs2.size)
+        {
+            res.classes[res.size] = substrate_equivalence_class_clone(ugs1.classes[i1]);
+            res.size++;
+        }
+    }
+
+    res_size = res.size;
+    for(i2=0 ; i2<ugs2.size ; i2++)
+    {
+        for(i=0 ; i<res_size ; i++)
+        {
+            if(substrate_access_class_eq(
+                        res.classes[i].array_references->elt,
+                        ugs2.classes[i2].array_references->elt))
+            {
+                break;
+            }
+        }
+        if(i >= res_size)
+        {
+            res.classes[res.size] = substrate_equivalence_class_clone(ugs2.classes[i2]);
+            res.size++;
+        }
+    }
+
+    return res;
+}
+
+struct substrate_equivalence_class substrate_equivalence_class_add(
+        struct substrate_equivalence_class ec1,
+        struct substrate_equivalence_class ec2)
+{
+    struct substrate_equivalence_class res;
+
+    res.size = ec1.size + ec2.size;
+    res.array_references = NULL;
+    osl_relation_list_add(
+            &res.array_references,
+            osl_relation_list_clone(ec1.array_references));
+    osl_relation_list_add(
+            &res.array_references,
+            osl_relation_list_clone(ec2.array_references));
+    
+    return res;
 }
